ae0ff94f12c676e1bb43da1ab7709379ca62267a
MJ Felix <mjfelixdev@gmail.com>
2021-04-03 18:56:48 +1300

    Complete coming soon version of the app

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.all('*', (req, res) => {
+    res.render('home');
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port} ...`)
+})
