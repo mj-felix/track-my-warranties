278521919d26911e4896434adc1bee9e4b9bdfd4
MJ Felix <mjfelixdev@gmail.com>
2021-04-05 13:54:17 +1200

    Add http to https redirection

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+//middleware: heroku http->https redirection
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https') {
+            res.redirect(301, 'https://trackmywarranties.mjfelix.dev')
+        }
+        else
+            next()
+    })
+}
+
 app.all('*', (req, res) => {
     res.render('home');
 })
