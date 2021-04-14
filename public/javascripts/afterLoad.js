25a3bfd4fa37a1c0bcf296254bd09f48c9ac06c1
MJ Felix <mjfelixdev@gmail.com>
2021-04-15 10:36:04 +1200

    Clean up flash message js

--- public/javascripts/afterLoad.js
+++ public/javascripts/afterLoad.js
@@ -3,21 +3,32 @@ const mainContainer = document.querySelector('#mainContainer');
 for (const cl of colOffsetClasses.split(' ')) mainContainer.classList.add(cl);
 
 // auto hide flash message
-const flash = document.getElementById('flashContainer');
-const intervalID = setInterval(function () {
+if (document.querySelector('#flashContainer div.alert')) {
+    const flash = document.getElementById('flashContainer');
+    const intervalID = setInterval(function () {
+        if (!flash.style.opacity) {
+            flash.style.opacity = 1;
+        }
+        if (flash.style.opacity >= 0) {
+            flash.style.opacity -= 0.1;
+        } else {
+            clearInterval(intervalID);
+            flash.remove();
+        }
+    }, 200);
+}
 
-    if (!flash.style.opacity) {
-        flash.style.opacity = 1;
+// add date pattern to date input label for Safari
+if (document.querySelector('span.datePattern')) {
+    const dateInputTest = document.createElement('input');
+    try {
+        dateInputTest.type = 'date';
+    } catch (e) {
+        console.log(e.description);
     }
-
-
-    if (flash.style.opacity >= 0) {
-        flash.style.opacity -= 0.1;
+    if (dateInputTest.type === 'text') {
+        for (let span of document.querySelectorAll('span.datePattern')) {
+            span.textContent = '(YYYY-MM-DD)';
+        }
     }
-
-    else {
-        clearInterval(intervalID);
-        flash.remove();
-    }
-
-}, 200);
+}
