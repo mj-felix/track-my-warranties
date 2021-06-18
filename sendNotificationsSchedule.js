5b2c50c5903741270e89eea87ac2b8741518bacf
MJ Felix <mjfelixdev@gmail.com>
2021-06-18 20:35:34 +1200

    Fix async sending of email notifications

--- sendNotificationsSchedule.js
+++ sendNotificationsSchedule.js
@@ -20,10 +20,7 @@ db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", async () => {
     console.log(`${new Date().toString()}: database connected`);
     const currentDate = new Date();
-    // const currentDate = new Date(Date.now() + 1000 * 60 * 60 * 24);
-    // console.log('Before set UTC:', currentDate);
     currentDate.setUTCHours(0, 0, 0, 0);
-    // console.log('After set UTC:', currentDate);
     const entries = await Entry.find({
         $or: [
             { date12weekNotification: currentDate },
@@ -33,21 +30,26 @@ db.once("open", async () => {
     }).populate('user');
     for (let entry of entries) {
         if (entry.date12weekNotification && entry.date12weekNotification.getTime() === currentDate.getTime()) {
-            await sendEmailNotification(entry.user.username, entry.productName, entry.dateExpired, entry._id);
-            entry.date12weekNotification = null;
-            await entry.save();
+            const isEmailSent = await sendEmailNotification(entry.user.username, entry.productName, entry.dateExpired, entry._id);
+            if (isEmailSent) {
+                entry.date12weekNotification = null;
+                await entry.save();
+            }
         }
         if (entry.date4weekNotification && entry.date4weekNotification.getTime() === currentDate.getTime()) {
-            await sendEmailNotification(entry.user.username, entry.productName, entry.dateExpired, entry._id);
-            entry.date4weekNotification = null;
-            await entry.save();
+            const isEmailSent = await sendEmailNotification(entry.user.username, entry.productName, entry.dateExpired, entry._id);
+            if (isEmailSent) {
+                entry.date4weekNotification = null;
+                await entry.save();
+            }
         }
         if (entry.date1weekNotification && entry.date1weekNotification.getTime() === currentDate.getTime()) {
-            await sendEmailNotification(entry.user.username, entry.productName, entry.dateExpired, entry._id);
-            entry.date1weekNotification = null;
-            await entry.save();
+            const isEmailSent = await sendEmailNotification(entry.user.username, entry.productName, entry.dateExpired, entry._id);
+            if (isEmailSent) {
+                entry.date1weekNotification = null;
+                await entry.save();
+            }
         }
     }
-    // console.log(entries);
     db.close();
 });
--- utils/sendEmailNotification.js
+++ utils/sendEmailNotification.js
@@ -3,26 +3,25 @@ const sgMail = require('@sendgrid/mail');
 
 module.exports = async (email, productName, dateExpired, entryId) => {
 
-    console.log(email, productName, dateExpired, entryId);
-
     if ('SENDGRID_API_KEY' in process.env) {
         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
         const url = process.env.PROD_URL || 'http://localhost:3000';
         const msg = {
             to: email,
             from: process.env.NO_RESPONSE_EMAIL,
-            subject: `[Track My Warranties] ${productName} expires on ${dateFormat(dateExpired, 'dd mmm yyyy')}`,
+            subject: `[TrackMyWarranties] ${productName} expires ${dateFormat(dateExpired, 'dd mmm yyyy')}`,
             text: `Warranty details:\n\n${url}/entries/${entryId}`,
         }
-        sgMail
-            .send(msg)
-            .then(() => {
-                console.log('Email sent')
-            })
-            .catch((error) => {
-                console.error(error)
-            })
+
+        try {
+            await sgMail.send(msg);
+            console.log(`Email sent to ${email}: ${productName} (${entryId}) expires ${dateExpired}`);
+            return true;
+        } catch (e) {
+            console.log(`Email NOT sent to ${email}: ${productName} (${entryId}) expires ${dateExpired}`);
+            console.error(e);
+        }
     }
 
-
+    return false;
 }
