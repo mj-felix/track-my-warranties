e87d4454ba4fee76a7ab3b30824cfbae58df9f30
MJ Felix <mjfelixdev@gmail.com>
2021-06-30 07:01:19 +1200

    Allow <br /> tag

--- models/validation.js
+++ models/validation.js
@@ -11,10 +11,10 @@ const extension = (joi) => ({
         escapeHTML: {
             validate(value, helpers) {
                 const clean = sanitizeHtml(value, {
-                    allowedTags: [],
+                    allowedTags: ['br'],
                     allowedAttributes: {},
                 });
-                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                if (clean !== value) return helpers.error('string.escapeHTML', { value });
                 return clean;
             }
         }
