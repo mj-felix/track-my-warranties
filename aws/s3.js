94056939ecedb48f145d98ad84f9c7d022632a4a
MJ Felix <mjfelixdev@gmail.com>
2022-05-17 20:32:25 +1200

    Create bucket on docker compose up in dev env

--- aws/s3.js
+++ aws/s3.js
@@ -1,13 +1,13 @@
 const AWS = require("aws-sdk");
 
 const s3Config = {
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_ACCESS_SECRET
-}
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_ACCESS_SECRET,
+};
 
-if (s3Config.accessKeyId === "minioadmin"){ 
-    s3Config.endpoint = `http://s3mock:${process.env.S3MOCK_PORT}`;
-    s3Config.s3ForcePathStyle = true;
+if (s3Config.accessKeyId === "minioadmin") {
+  s3Config.endpoint = `http://s3mock:${process.env.S3MOCK_PORT}`;
+  s3Config.s3ForcePathStyle = true;
 }
 
 const s3 = new AWS.S3(s3Config);
