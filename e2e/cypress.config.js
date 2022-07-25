3da6c63b17f334c50802bb51cd5c5d3a39f09a88
MJ Felix <mjfelixdev@gmail.com>
2022-07-25 23:06:08 +1200

    Format cypress config file

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     supportFile: false,
-    specPattern: 'cypress/specs/**/*.cy.js',
-    video: false
-  }
-})
+    specPattern: "cypress/specs/**/*.cy.js",
+    video: false,
+  },
+});
